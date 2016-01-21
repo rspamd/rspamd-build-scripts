0cd0abba8a2b9f96bf4fe62cd9d8d8be67bf9f24
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-01-21 15:13:03 +0000

    Rework build scripts for debian

---
+++ config.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+
+DEPS_DEB="fakeroot make ca-certificates less git vim devscripts debhelper \
+    dpkg-dev cmake libevent-dev libglib2.0-dev libgmime-2.6-dev libpcre3-dev \
+    libssl-dev libcurl4-openssl-dev libsqlite3-dev perl libopendkim-dev \
+    libmilter-dev libspf2-dev bison flex libmagic-dev git ragel libfann-dev"
+export DISTRIBS_DEB="ubuntu-precise \
+  ubuntu-trusty \
+  ubuntu-vivid \
+  ubuntu-wily \
+  debian-jessie \
+  debian-wheezy"
+export DISTRIBS_RPM="centos-6 centos-7 fedora-22"
+
+export RSPAMD_VER_UNSTABLE="1.1.1"
+export RSPAMD_VER_STABLE="1.1.0"
+export RMILTER_VER_UNSTABLE="1.7.1"
+export RMILTER_VER_STABLE="1.7.1"
+# RPM stupidity
+export BUILD_DIR="/build7558b18c49c3aede6aa20ecb0513b9eb2b39ce7db0c739ec006369009fdf893d91b9ec4199fa64acd80aa1de7fac87a148a6f65e98f258b455996c5f99d990d2"
+
+if [ -n "${STABLE}" ] ; then
+	export RSPAMD_VER="${RSPAMD_VER_STABLE}"
+	export RMILTER_VER="${RMILTER_VER_STABLE}"
+	export STABLE_VER="1"
+else
+	export RSPAMD_VER="${RSPAMD_VER_UNSTABLE}"
+	export RMILTER_VER="${RMILTER_VER_UNSTABLE}"
+fi
+
+export KEY="3EF4A6C1"
+export TARGET_DEB_STABLE="rspamd.com/apt-stable"
+export TARGET_DEB_UNSTABLE="rspamd.com/apt"
+export TARGET_RPM_STABLE="rspamd.com/rpm-stable"
+export TARGET_RPM_UNSTABLE="rspamd.com/rpm"
+export SSH_KEY_DEB_STABLE="${HOME}/.ssh/identity.repo-deb-stable"
+export SSH_KEY_DEB_UNSTABLE="${HOME}/.ssh/identity.repo-deb-unstable"
+export SSH_KEY_RPM_STABLE="${HOME}/.ssh/identity.repo-rpm-stable"
+export SSH_KEY_RPM_UNSTABLE="${HOME}/.ssh/identity.repo-rpm-unstable"
+
+if [ -f "./config.local.sh" ] ; then
+  . ./config.local.sh
+fi
---
+++ rspamd_build.sh
@@ -0,0 +1,424 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND="noninteractive"
+export LANG="C"
+DEBIAN=0
+RPM=0
+DEPS_STAGE=0
+BUILD_STAGE=0
+SIGN_STAGE=0
+UPLOAD_STAGE=0
+
+usage()
+{
+	echo "Rspamd build packages script"
+	echo ""
+	echo "./rspamd_build.sh"
+	echo "\t-h --help"
+	echo "\t--all: do all stages"
+	echo "\t--deb: build debian packages"
+	echo "\t--rpm: build rpm packages"
+	echo "\t--stable: build stable packages"
+	echo "\t--deps: install dependencies"
+	echo "\t--build: do build step"
+	echo "\t--sign: do sign step"
+	echo "\t--upload: upload packages using ssh"
+	echo "\t--upload-host: use the following upload host"
+	echo "\t--no-inc: do not increase version for rolling release"
+	echo "\t--no-i386: do not build packages for i386"
+	echo "\t--no-rmilter: do not build rmilter packages"
+	echo "\t--no-rspamd: do not build rspamd packages"
+	echo "\t--no-hyperscan: do not use hyperscan"
+	echo ""
+}
+
+while [ "$1" != "" ]; do
+	PARAM=`echo $1 | awk -F= '{print $1}'`
+	VALUE=`echo $1 | awk -F= '{print $2}'`
+	case $PARAM in
+		-h | --help)
+			usage
+			exit
+			;;
+		--deb)
+			DEBIAN=1
+			;;
+		--rpm)
+			RPM=1
+			;;
+		--stable)
+			export STABLE=1
+			;;
+		--all)
+			DEPS_STAGE=1
+			BUILD_STAGE=1
+			SIGN_STAGE=1
+			UPLOAD_STAGE=1
+			;;
+		--deps)
+			DEPS_STAGE=1
+			;;
+		--build)
+			BUILD_STAGE=1
+			;;
+		--sign)
+			SIGN_STAGE=1
+			;;
+		--upload)
+			UPLOAD_STAGE=1
+			;;
+		--no-inc)
+			NO_INC=1
+			;;
+		--no-i386)
+			NO_I386=1
+			;;
+		--no-rmilter)
+			NO_RMILTER=1
+			;;
+		--no-rspamd)
+			NO_RSPAMD=1
+			;;
+		--upload-host)
+			UPLOAD_HOST="${VALUE}"
+			;;
+		*)
+			echo "ERROR: unknown parameter \"$PARAM\""
+			usage
+			exit 1
+			;;
+	esac
+	shift
+done
+
+. ./config.sh
+
+get_rspamd() {
+	rm -fr ${HOME}/rspamd ${HOME}/rspamd.build ${HOME}/rmilter ${HOME}/rmilter.build
+	/usr/bin/git clone --recursive https://github.com/vstakhov/rspamd ${HOME}/rspamd
+	/usr/bin/git clone --recursive https://github.com/vstakhov/rmilter ${HOME}/rmilter
+
+	if [ -n "${STABLE}" ] ; then
+		( cd ${HOME}/rspamd && git checkout ${RSPAMD_VER} )
+		( cd ${HOME}/rmilter && git checkout ${RMILTER_VER} )
+	fi
+
+	( mkdir ${HOME}/rspamd.build ; cd ${HOME}/rspamd.build ; cmake ${HOME}/rspamd ; make dist ) 
+	( mkdir ${HOME}/rmilter.build ; cd ${HOME}/rmilter.build ; cmake ${HOME}/rmilter ; make dist ) 
+}
+
+do_dir() {
+	d=$1
+	#rm -fr ${HOME}/$d/opt/hyperscan
+	rm -f ${HOME}/$d/*.deb
+	rm -f ${HOME}/$d/*.debian.tar.gz
+	rm -f ${HOME}/$d/*.changes
+	rm -f ${HOME}/$d/*.dsc
+	rm -f ${HOME}/$d/*.build
+
+	chroot ${HOME}/$d "/usr/bin/apt-get" update
+	chroot ${HOME}/$d "/usr/bin/apt-get" install -y --no-install-recommends ${REAL_DEPS}
+	if [ -n "${HYPERSCAN}" -a -z "${NO_HYPERSCAN}" ] ; then
+		echo $d | grep 'i386' > /dev/null
+		if [ $? -ne 0 ] ; then
+			#rm -fr ${HOME}/$d/opt/hyperscan
+			if [ ! -d ${HOME}/$d/opt/hyperscan ] ; then
+				rm -fr ${HOME}/$d/hyperscan ${HOME}/$d/hyperscan.build
+				chroot ${HOME}/$d "/usr/bin/git" clone https://github.com/01org/hyperscan.git
+				if [ $? -ne 0 ] ; then
+					exit 1
+				fi
+				( cd ${HOME}/$d ; tar xzf ${HOME}/boost.tar.gz )
+				mkdir ${HOME}/$d/hyperscan.build
+				chroot ${HOME}/$d "/bin/sh" -c "cd /hyperscan.build ; cmake ../hyperscan -DCMAKE_INSTALL_PREFIX=/opt/hyperscan -DBOOST_ROOT=/boost_1_59_0 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_C_FLAGS=-march=core2 -DCMAKE_CXX_FLAGS=-march=core2 && make -j2 && make install"
+				if [ $? -ne 0 ] ; then
+					exit 1
+				fi
+			else
+				# cleanup build
+				rm -fr ${HOME}/$d/hyperscan ${HOME}/$d/hyperscan.build ${HOME}/$d/boost_1_59_0 || true
+			fi
+		fi
+	fi
+	cp ${HOME}/rspamd.build/rspamd-${RSPAMD_VER}.tar.xz ${HOME}/$d/
+	cp ${HOME}/rmilter.build/rmilter-${RMILTER_VER}.tar.xz ${HOME}/$d/
+}
+
+if [ $DEPS_STAGE -eq 1 ] ; then
+	get_rspamd
+	for d in $DISTRIBS_DEB ; do
+		HYPERSCAN=""
+		grep universe ${HOME}/$d/etc/apt/sources.list > /dev/null 2>&1
+		if [ $? -ne 0 ] ; then
+			sed -e 's/main/main universe/' ${HOME}/$d/etc/apt/sources.list > /tmp/.tt
+			mv /tmp/.tt ${HOME}/$d/etc/apt/sources.list
+		fi
+
+		case $d in
+			debian-jessie) REAL_DEPS="$DEPS dh-systemd libluajit-5.1-dev" HYPERSCAN="yes";;
+			debian-wheezy) REAL_DEPS="$DEPS liblua5.1-dev" ;;
+			ubuntu-precise) REAL_DEPS="$DEPS libluajit-5.1-dev" ;;
+			ubuntu-*) 
+				#chroot ${HOME}/$d /bin/sh -c "sed -e 's/main/main universe/' /etc/apt/sources.list > /tmp/.tt ; mv /tmp/.tt /etc/apt/sources.list"
+				REAL_DEPS="$DEPS libluajit-5.1-dev" 
+				HYPERSCAN="yes"
+				;;
+			*) REAL_DEPS="$DEPS libluajit-5.1-dev" HYPERSCAN="yes" ;;
+		esac
+
+		do_dir $d
+
+### i386 ###
+		if [ -z "${NO_I386}" ] ; then
+			d="$d-i386"
+			do_dir $d
+		fi
+	done
+fi
+
+build_rspamd() {
+	d=$1
+
+	echo "******* BUILD RSPAMD ${RSPAMD_VER} FOR $d ********"
+
+	_id=`git -C ${HOME}/rspamd rev-parse --short HEAD` 
+	_distname=`echo $d | sed -r -e 's/ubuntu-|debian-//' -e 's/-i386//'`
+	_deps_line=`echo ${REAL_DEPS} | tr ' ' ','`
+	if [ -n "${STABLE}" ] ; then
+		RULES_SED="${RULES_SED} -e \"s/-DDEBIAN_BUILD=1/-DDEBIAN_BUILD=1/\""
+	else
+		RULES_SED="${RULES_SED} -e \"s/-DDEBIAN_BUILD=1/-DDEBIAN_BUILD=1 -DGIT_ID=${_id}/\""
+	fi
+	chroot ${HOME}/$d sh -c "rm -fr rspamd-${RSPAMD_VER} ; tar xvf rspamd-${RSPAMD_VER}.tar.xz"
+	chroot ${HOME}/$d sh -c "cp rspamd-${RSPAMD_VER}.tar.xz rspamd_${RSPAMD_VER}.orig.tar.xz"
+	chroot ${HOME}/$d sh -c "sed -e \"s/Build-Depends:.*/Build-Depends: ${_deps_line}/\" -e \"s/Maintainer:.*/Maintainer: Vsevolod Stakhov <vsevolod@highsecure.ru>/\" < rspamd-${RSPAMD_VER}/debian/control > /tmp/.tt ; mv /tmp/.tt rspamd-${RSPAMD_VER}/debian/control"
+	if [ -n "${STABLE}" ] ; then
+		chroot ${HOME}/$d sh -c "sed -e \"s/unstable/${_distname}/\" \
+	  -e \"s/Mikhail Gusarov <dottedmag@debian.org>/Vsevolod Stakhov <vsevolod@highsecure.ru>/\" \
+	  -e \"s/1.0.2/${RSPAMD_VER}-${_version}~${_distname}/\" < rspamd-${RSPAMD_VER}/debian/changelog > /tmp/.tt ; \
+	   mv /tmp/.tt rspamd-${RSPAMD_VER}/debian/changelog"
+	else
+		chroot ${HOME}/$d sh -c "sed -e \"s/unstable/${_distname}/\" \
+	  -e \"s/Mikhail Gusarov <dottedmag@debian.org>/Vsevolod Stakhov <vsevolod@highsecure.ru>/\" \
+	  -e \"s/1.0.2/${RSPAMD_VER}-0~git${_version}~${_id}~${_distname}/\" \
+	  < rspamd-${RSPAMD_VER}/debian/changelog > /tmp/.tt ; \
+	  mv /tmp/.tt rspamd-${RSPAMD_VER}/debian/changelog"
+	fi
+	if [ -n "$RULES_SED" ] ; then
+		chroot ${HOME}/$d sh -c "sed ${RULES_SED} < rspamd-${RSPAMD_VER}/debian/rules > /tmp/.tt ; \
+	  mv /tmp/.tt rspamd-${RSPAMD_VER}/debian/rules"
+	fi
+	chroot ${HOME}/$d sh -c "sed -e 's/native/quilt/' < rspamd-${RSPAMD_VER}/debian/source/format > /tmp/.tt ; \
+	  mv /tmp/.tt rspamd-${RSPAMD_VER}/debian/source/format"
+	chroot ${HOME}/$d sh -c "cd rspamd-${RSPAMD_VER} ; debuild -us -uc"
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+}
+
+build_rmilter() {
+	d=$1
+	echo "******* BUILD RMILTER ${RMILTER_VER} FOR $d ********"
+
+	_id=`git -C ${HOME}/rmilter rev-parse --short HEAD` 
+	_distname=`echo $d | sed -r -e 's/ubuntu-|debian-//' -e 's/-i386//'`
+	_deps_line=`echo ${REAL_DEPS} | tr ' ' ','`
+	chroot ${HOME}/$d sh -c "rm -fr rmilter-${RMILTER_VER} ; tar xvf rmilter-${RMILTER_VER}.tar.xz"
+	chroot ${HOME}/$d sh -c "cp rmilter-${RMILTER_VER}.tar.xz rmilter_${RMILTER_VER}.orig.tar.xz"
+	chroot ${HOME}/$d sh -c "sed -e \"s/Build-Depends:.*/Build-Depends: ${_deps_line}/\" \
+	  -e \"s/Maintainer:.*/Maintainer: Vsevolod Stakhov <vsevolod@highsecure.ru>/\" \
+	  < rmilter-${RMILTER_VER}/debian/control > /tmp/.tt ; \
+	  mv /tmp/.tt rmilter-${RMILTER_VER}/debian/control"
+	if [ -n "${STABLE}" ] ; then
+		chroot ${HOME}/$d sh -c "sed -e \"s/unstable/${_distname}/\" \
+	  -e \"s/Mikhail Gusarov <dottedmag@debian.org>/Vsevolod Stakhov <vsevolod@highsecure.ru>/\" \
+	  -e \"s/1.6.[0-9]*/${RMILTER_VER}-${_version}~${_distname}/\" \
+	  < rmilter-${RMILTER_VER}/debian/changelog > /tmp/.tt ; \
+	  mv /tmp/.tt rmilter-${RMILTER_VER}/debian/changelog"
+	else
+		chroot ${HOME}/$d sh -c "sed -e \"s/unstable/${_distname}/\" \
+	  -e \"s/Mikhail Gusarov <dottedmag@debian.org>/Vsevolod Stakhov <vsevolod@highsecure.ru>/\" \
+	  -e \"s/1.6.[0-9]*/${RMILTER_VER}-0~git${_version}~${_id}~${_distname}/\" \
+	  < rmilter-${RMILTER_VER}/debian/changelog > /tmp/.tt ; \
+	  mv /tmp/.tt rmilter-${RMILTER_VER}/debian/changelog"
+	fi
+	if [ -n "$RULES_SED" ] ; then
+		chroot ${HOME}/$d sh -c "sed ${RULES_SED} < rmilter-${RMILTER_VER}/debian/rules > /tmp/.tt ; \
+	  mv /tmp/.tt rmilter-${RMILTER_VER}/debian/rules"
+	fi
+	chroot ${HOME}/$d sh -c "sed -e 's/native/quilt/' < rmilter-${RMILTER_VER}/debian/source/format > /tmp/.tt ; \
+	  mv /tmp/.tt rmilter-${RMILTER_VER}/debian/source/format"
+	chroot ${HOME}/$d sh -c "cd rmilter-${RMILTER_VER} ; debuild -us -uc"
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+}
+
+
+if [ $BUILD_STAGE -eq 1 ] ; then
+
+	if [ -n "${STABLE}" ] ; then
+		export DEB_BUILD_OPTIONS="parallel=8"
+		_version="${STABLE_VER}"
+	else
+		export DEB_BUILD_OPTIONS="parallel=8 nostrip"
+		_version=`cat ${HOME}/version || echo 0`
+		if [ $# -ge 1 ] ; then
+			DISTRIBS=$@
+		else
+			_version=$(($_version + 1))
+		fi
+	fi
+
+	if [ -z "${NO_RSPAMD}" ] ; then
+	for d in $DISTRIBS_DEB ; do
+		case $d in
+		debian-jessie) REAL_DEPS="$DEPS dh-systemd libluajit-5.1-dev" RULES_SED="-e 's/--with-systemd/--with-systemd --parallel/' -e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=ON -DENABLE_HYPERSCAN=ON -DHYPERSCAN_ROOT_DIR=\/opt\/hyperscan -DENABLE_FANN=ON/'" ;;
+		debian-wheezy) REAL_DEPS="$DEPS liblua5.1-dev" RULES_SED="-e 's/--with systemd/--parallel/' -e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=OFF -DENABLE_FANN=ON/'" ;;
+		ubuntu-precise) 
+			REAL_DEPS="$DEPS libluajit-5.1-dev"
+			RULES_SED="-e 's/--with systemd/--parallel/' -e \
+			's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=OFF -DENABLE_FANN=ON/'"
+			;;
+		ubuntu-wily) 
+			REAL_DEPS="$DEPS libluajit-5.1-dev"
+			RULES_SED="-e 's/--with systemd/--parallel/' \
+			-e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=ON -DENABLE_HYPERSCAN=ON -DHYPERSCAN_ROOT_DIR=\/opt\/hyperscan -DENABLE_FANN=ON/'"
+			;;
+		ubuntu-*) 
+			REAL_DEPS="$DEPS libluajit-5.1-dev"
+			RULES_SED="-e 's/--with systemd/--parallel/' \
+			-e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=OFF -DENABLE_HYPERSCAN=ON -DHYPERSCAN_ROOT_DIR=\/opt\/hyperscan -DENABLE_FANN=ON/'"
+			;;
+		*) REAL_DEPS="$DEPS libluajit-5.1-dev" ;;
+		esac
+		build_rspamd $d
+
+### i386 ###
+	  if [ -z "${NO_I386}" ] ; then
+		d="${d}-i386"
+		build_rspamd $d
+	  fi
+	done
+  fi
+
+  RULES_SED=""
+
+  if [ -z "${NO_RMILTER}" ] ; then
+		for d in $DISTRIBS_DEB ; do
+			case $d in
+				debian-jessie) REAL_DEPS="$DEPS dh-systemd" RULES_SED="-e 's/--with-systemd/--with-systemd --parallel/'" ;;
+				debian-wheezy) REAL_DEPS="$DEPS" RULES_SED="-e 's/--with systemd/--parallel/' -e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=OFF/'" ;;
+				ubuntu-*) 
+					REAL_DEPS="$DEPS"
+					RULES_SED="-e 's/--with systemd/--parallel/' -e 's/-DWANT_SYSTEMD_UNITS=ON/-DWANT_SYSTEMD_UNITS=OFF/'"
+					;;
+				*) REAL_DEPS="$DEPS" ;;
+			esac
+
+			build_rmilter $d
+### i386 ###
+			if [ -z "${NO_I386}" ] ; then
+				d="${d}-i386"
+				build_rmilter $d
+			fi
+		done
+	fi
+
+# Increase version
+	if [ -z "${STABLE}" -a -z "${NO_INC}" ] ; then
+		echo $_version > ${HOME}/version
+	fi
+fi
+
+if [ ${SIGN_STAGE} -eq 1 ] ; then
+
+	if [ -n "${STABLE}" ] ; then
+		_version="${STABLE_VER}"
+	else
+		_version=`cat ${HOME}/version || echo 0`
+	fi
+	
+	_id=`git -C ${HOME}/rspamd rev-parse --short HEAD`
+	_rmilter_id=`git -C ${HOME}/rmilter rev-parse --short HEAD`
+	rm -fr ${HOME}/repos/*
+	gpg --armor --output ${HOME}/repos/gpg.key --export $KEY
+	mkdir ${HOME}/repos/conf || true
+
+	for d in $DISTRIBS_DEB ; do
+		_distname=`echo $d | sed -r -e 's/ubuntu-|debian-//'`
+		if [ -n "${STABLE}" ] ; then
+			_pkg_ver="${RSPAMD_VER}-${_version}~${_distname}"
+			_rmilter_pkg_ver="${RMILTER_VER}-${_version}~${_distname}"
+			_repo_descr="Apt repository for rspamd stable builds"
+		else
+			_pkg_ver="${RSPAMD_VER}-0~git${_version}~${_id}~${_distname}"
+			_rmilter_pkg_ver="${RMILTER_VER}-0~git${_version}~${_rmilter_id}~${_distname}"
+			_repo_descr="Apt repository for rspamd nightly builds"
+		fi
+		if [ -z "${NO_I386}" ] ; then
+			ARCHS="source amd64"
+		else
+			ARCHS="source amd64 i386"
+		fi
+		_repodir=${HOME}/repos/
+		cat >> $_repodir/conf/distributions <<EOD
+Origin: Rspamd
+Label: Rspamd
+Codename: ${_distname}
+Architectures:  ${ARCHS}
+Components: main
+Description: ${_repo_descr}
+SignWith: ${KEY}
+
+EOD
+	if [ -z "${NO_RSPAMD}" ] ; then
+		dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rspamd_${_pkg_ver}*.deb
+		dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rspamd-dbg_${_pkg_ver}*.deb
+		debsign --re-sign -k $KEY ${HOME}/$d/rspamd_${_pkg_ver}*.changes
+		reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rspamd_${_pkg_ver}_amd64.deb
+		reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rspamd-dbg_${_pkg_ver}_amd64.deb
+		reprepro -b $_repodir -v --keepunreferencedfiles includedsc $_distname $d/rspamd_${_pkg_ver}.dsc
+	fi
+	
+	if [ -z "${NO_RMILTER}" ] ; then
+			dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rmilter_${_rmilter_pkg_ver}*.deb
+			dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rmilter-dbg_${_rmilter_pkg_ver}*.deb
+			debsign --re-sign -k $KEY ${HOME}/$d/rmilter_${_rmilter_pkg_ver}*.changes
+			reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rmilter_${_rmilter_pkg_ver}_amd64.deb
+			reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rmilter-dbg_${_rmilter_pkg_ver}_amd64.deb
+			reprepro -b $_repodir -v --keepunreferencedfiles includedsc $_distname $d/rmilter_${_rmilter_pkg_ver}.dsc
+		fi
+### i386 ###
+		if [ -z "${NO_I386}" ] ; then
+		d="${d}-i386"
+		if [ -z "${NO_RSPAMD}" ] ; then
+			dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rspamd_${_pkg_ver}*.deb
+			dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rspamd-dbg_${_pkg_ver}*.deb
+			reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rspamd_${_pkg_ver}_i386.deb
+			reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rspamd-dbg_${_pkg_ver}_i386.deb
+			debsign --re-sign -k $KEY ${HOME}/$d/rspamd_${_pkg_ver}*.changes
+		fi
+		dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rmilter_${_rmilter_pkg_ver}*.deb
+		dpkg-sig -k $KEY --batch=1 --sign builder ${HOME}/$d/rmilter-dbg_${_rmilter_pkg_ver}*.deb
+		debsign --re-sign -k $KEY ${HOME}/$d/rmilter_${_rmilter_pkg_ver}*.changes
+		reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rmilter_${_rmilter_pkg_ver}_i386.deb
+		reprepro -b $_repodir -v --keepunreferencedfiles includedeb $_distname $d/rmilter-dbg_${_rmilter_pkg_ver}_i386.deb
+	fi
+
+	gpg -u 0x$KEY -sb $_repodir/dists/$_distname/Release && \
+		mv $_repodir/dists/$_distname/Release.sig $_repodir/dists/$_distname/Release.gpg
+	done
+fi
+
+if [ ${UPLOAD_STAGE} -eq 1 ] ; then
+	if [ -z "${UPLOAD_HOST}" ] ; then
+		echo "No UPLOAD_HOST specified, exiting"
+		exit 1
+	fi
+	if [ -n "${STABLE}" ] ; then
+		scp -r -i ${SSH_KEY_DEB_STABLE}  ${HOME}/repos/* ${UPLOAD_HOST}:${TARGET_DEB_STABLE}
+	else
+		scp -r -i ${SSH_KEY_DEB_UNSTABLE}  ${HOME}/repos/* ${UPLOAD_HOST}:${TARGET_DEB_UNSTABLE}
+	fi
+fi
