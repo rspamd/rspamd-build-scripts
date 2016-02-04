7e1b66efa099ed5a0d83705f4311e7a4d977dea5
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-02-04 22:50:56 +0000

    Update versions

--- config.sh
+++ config.sh
@@ -17,10 +17,10 @@ export DEPS_RPM="glib2-devel libevent-devel openssl-devel pcre-devel perl \
 	hiredis-devel cmake rpm-build gmime-devel gcc make sendmail-devel bison \
 	flex file-devel fann-devel git rsync perl-Digest-MD5"
 
-export RSPAMD_VER_UNSTABLE="1.1.3"
-export RSPAMD_VER_STABLE="1.1.2"
-export RMILTER_VER_UNSTABLE="1.7.3"
-export RMILTER_VER_STABLE="1.7.2"
+export RSPAMD_VER_UNSTABLE="1.2.0"
+export RSPAMD_VER_STABLE="1.1.3"
+export RMILTER_VER_UNSTABLE="1.7.4"
+export RMILTER_VER_STABLE="1.7.3"
 # RPM stupidity
 export BUILD_DIR="/build7558b18c49c3aede6aa20ecb0513b9eb2b39ce7db0c739ec006369009fdf893d91b9ec4199fa64acd80aa1de7fac87a148a6f65e98f258b455996c5f99d990d2"
 
