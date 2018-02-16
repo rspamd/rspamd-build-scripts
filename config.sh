8b8044314e8463b2d3a30d2f7d433a58adf6f24c
Vsevolod Stakhov <vsevolod@highsecure.ru>
2018-02-16 13:50:29 +0000

    Update to 1.6.6

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ export DEPS_RPM="glib2-devel libevent-devel openssl-devel pcre-devel perl \
 
 export MAIN_ARCH="x86_64"
 export RSPAMD_VER_UNSTABLE="1.7.0"
-export RSPAMD_VER_STABLE="1.6.5"
+export RSPAMD_VER_STABLE="1.6.6"
 export RMILTER_VER_UNSTABLE="2.0.0"
 export RMILTER_VER_STABLE="1.10.0"
 # RPM stupidity
