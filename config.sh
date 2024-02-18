79e321bb20e59d877e4f3d7e706804c26ab07a5d
Vsevolod Stakhov <vsevolod@rspamd.com>
2024-02-18 22:18:47 +0000

    Update versions

--- config.sh
+++ config.sh
@@ -29,8 +29,8 @@ export UPLOAD_SUFFIX="rspamd.com/dist/"
 export SSH_HOST_X86="example.com"
 export SSH_HOST_AARCH64="example.com"
 
-export RSPAMD_VER_UNSTABLE="3.8.1"
-export RSPAMD_VER_STABLE="3.8.0"
+export RSPAMD_VER_UNSTABLE="3.8.2"
+export RSPAMD_VER_STABLE="3.8.1"
 
 if [ -n "${STABLE}" ] ; then
   export RSPAMD_VER="${RSPAMD_VER_STABLE}"
