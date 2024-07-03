4e2c6ac3075af9c111449ca8aca22d6b40248198
Andrew Lewis <nerf@judo.za.org>
2024-07-03 17:06:35 +0200

    Drop CentOS 7

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export DISTRIBS_DEB="ubuntu-focal \
   ubuntu-noble \
   debian-bullseye \
   debian-bookworm"
-export DISTRIBS_RPM="centos-7 centos-8 centos-9"
+export DISTRIBS_RPM="centos-8 centos-9"
 # Old distributives that are just broken on aarch64 (e.g. old libc++)
 export ARM_BLACKLIST="ubuntu-bionic debian-buster"
 
